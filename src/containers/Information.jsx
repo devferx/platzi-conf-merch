b5b1e7462f94db979a133bd6aba00bb7a5e8e855
devferx <ferkki@hotmail.com>
2020-11-25 12:15:30 -0400

    Clase Integración de íconos y conexión con React Router

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Information.css';
 
 const Information = () => {
@@ -23,7 +24,9 @@ const Information = () => {
         </div>
         <div className="Information-buttons">
           <div className="Information-back">Regresar</div>
-          <div className="Information-next">pagar</div>
+          <div className="Information-next">
+            <Link to="/checkout/payment">pagar</Link>
+          </div>
         </div>
       </div>
       <div className="Information-sidebar">
