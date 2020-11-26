ca2b9558857bb5db0b4971d858f307647f7a3e9d
devferx <ferkki@hotmail.com>
2020-11-26 12:39:56 -0400

    [fixed] dollar spaces in title of product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,7 +6,7 @@ const Product = ({ product, handleAddToCart }) => {
       <img src={product.image} alt={product.title} />
       <div className="Product-item-info">
         <h2>
-          {product.title} <span>$ {product.price}</span>
+          {product.title} <span>${product.price}</span>
         </h2>
         <p>{product.description}</p>
       </div>
