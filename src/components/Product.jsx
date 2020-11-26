b0697e06bd6510ab7d649710e7549a5563905b9a
devferx <ferkki@hotmail.com>
2020-11-26 12:19:55 -0400

    [add] underscore

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,7 +7,7 @@ const Product = ({ product, handleAddToCart }) => {
       <div className="Product-item-info">
         <h2>
           {product.title}
-          <span>$ {product.price}</span>
+          <span>$ {product.price} _</span>
         </h2>
         <p>{product.description}</p>
       </div>
