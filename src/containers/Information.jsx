14081cda539cd0f7bc05596ace3390db28a05bcf
devferx <ferkki@hotmail.com>
2020-11-25 20:05:47 -0400

    Clase Integrando third party custom hooks en Platzi Conf Merch

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,11 +1,13 @@
 import React, { useRef, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
 import AppContext from '../context/AppContext';
 import '../styles/components/Information.css';
 
 const Information = () => {
   const { state, addToBuyer } = useContext(AppContext);
   const form = useRef(null);
+  const history = useHistory();
 
   const { cart } = state;
 
@@ -24,6 +26,7 @@ const Information = () => {
     };
 
     addToBuyer(buyer);
+    history.push('/checkout/payment');
   };
 
   return (
