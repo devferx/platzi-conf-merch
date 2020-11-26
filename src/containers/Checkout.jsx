42b58ca65f626cfb52a22f344d5161322b9fbb0b
devferx <ferkki@hotmail.com>
2020-11-26 11:00:50 -0400

    [add] react-helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import AppContext from '../context/AppContext';
 import '../styles/components/Checkout.css';
 
@@ -19,34 +20,40 @@ const Checkout = () => {
   };
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        <h3>{cart.length > 0 ? 'Lista de Pedidos:' : 'Sin pedidos...'}</h3>
-        {cart.map((item, index) => (
-          <div
-            key={`${item.title}-${Math.round(Math.random() * 1000, 1)}`}
-            className="Checkout-item"
-          >
-            <div className="Checkout-element">
-              <h4>{item.title}</h4>
-              <span>$ {item.price}</span>
-            </div>
-            <button type="button" onClick={handleRemove(index)}>
-              <i className="fas fa-trash-alt" />
-            </button>
-          </div>
-        ))}
-      </div>
+    <>
+      <Helmet>
+        <title>Lista de Pedidos - Platzi Conf Merch</title>
+      </Helmet>
 
-      {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continuar pedido</button>
-          </Link>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          <h3>{cart.length > 0 ? 'Lista de Pedidos:' : 'Sin pedidos...'}</h3>
+          {cart.map((item, index) => (
+            <div
+              key={`${item.title}-${Math.round(Math.random() * 1000, 1)}`}
+              className="Checkout-item"
+            >
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>$ {item.price}</span>
+              </div>
+              <button type="button" onClick={handleRemove(index)}>
+                <i className="fas fa-trash-alt" />
+              </button>
+            </div>
+          ))}
         </div>
-      )}
-    </div>
+
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continuar pedido</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Products from '../components/Products';
 import initialState from '../initialState';
 
 const Home = () => {
-  return <Products products={initialState.products} />;
+  return (
+    <>
+      <Helmet>
+        <title>Platzi Conf Merch - Productos</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@devferx" />
+        <meta name="twitter:creator" content="@devferx" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Platzi Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content="Platzi Conf Store" />
+        <meta property="og:description" content="Platzi Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta
+          property="og:url"
+          content="https://platzi-conf-merchandising.web.app"
+        />
+        <meta property="og:site_name" content="Platzi Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        {/* <meta property="fb:app_id" content="ID_APP_FACEBOOK" /> */}
+      </Helmet>
+      <Products products={initialState.products} />
+    </>
+  );
 };
 
 export default Home;
